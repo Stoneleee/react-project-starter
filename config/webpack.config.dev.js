580d8afe00d90186843b53087f748e32c51d529d
Stoneleee <weflylee@163.com>
2018-01-15 13:08:27 +0800

    添加css-modues

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -193,6 +193,8 @@ module.exports = {
             loader: require.resolve('css-loader'),
             options: {
               importLoaders: 1,
+              modules: true,
+              localIdentName: "[local]___[hash:base64:5]"
             },
           },
           {
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -198,6 +198,7 @@ module.exports = {
                   loader: require.resolve('css-loader'),
                   options: {
                     importLoaders: 1,
+                    modules: true,
                     minimize: true,
                     sourceMap: true,
                   },
